c0e604323b2dcabeea475adc1cb29dc40a27144b
syuan <syuan.jhong1989@gmail.com>
2022-06-15 15:57:53 +0800

    make it better

--- model.py
+++ model.py
@@ -3,35 +3,32 @@ import pandas as pd
 from to_sequences_1 import to_sequences
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import load_model
-def modell():
-    model = load_model('fog_model.h5')
-    lookback = 72
 
-    predictdata = pd.read_csv('./testdata.csv')
-    predictdata.drop(predictdata.index[predictdata[(predictdata.values == 'X') | \
-        (predictdata.values == 'V')].index], inplace=True)
-    predictdata = predictdata.drop(['time','Label'],axis=1)
-    predictdata = predictdata.astype("float64")
-    predictdata.info()
+model = load_model('fog_model.h5')
+lookback = 72
+predictdata = pd.read_csv('./testdata.csv')
+predictdata.drop(predictdata.index[predictdata[(predictdata.values == 'X') | \
+    (predictdata.values == 'V')].index], inplace=True)
+predictdata = predictdata.drop(['time','Label'],axis=1)
+predictdata = predictdata.astype("float64")
+predictdata.info()
 
-    scalerPredict = MinMaxScaler(feature_range=(0, 1))  # Also try QuantileTransformer
-    scaleredPredict = scalerPredict.fit_transform(predictdata)
+scalerPredict = MinMaxScaler(feature_range=(0, 1))  # Also try QuantileTransformer
+scaleredPredict = scalerPredict.fit_transform(predictdata)
 
-    scalerYPredict = MinMaxScaler(feature_range=(0, 1))
-    YPredict = scalerYPredict.fit_transform(predictdata['VIZ'].values.reshape(-1, 1))
-    scaleredPredict.shape, YPredict.shape
+scalerYPredict = MinMaxScaler(feature_range=(0, 1))
+YPredict = scalerYPredict.fit_transform(predictdata['VIZ'].values.reshape(-1, 1))
+scaleredPredict.shape, YPredict.shape
 
-    predictX, predictY = to_sequences(predictdata.values, lookback)
+predictX, predictY = to_sequences(predictdata.values, lookback)
     
-    predictX.shape, predictY.shape
+predictX.shape, predictY.shape
 
-    predictX = np.reshape(predictX, (predictX.shape[0], 1, -1))
+predictX = np.reshape(predictX, (predictX.shape[0], 1, -1))
 
-    threeDayPredict = model.predict(predictX)
-    threeDayPredict.shape
-    threeDayPredictOutput = scalerYPredict.inverse_transform(threeDayPredict)
-    print(threeDayPredictOutput)
-    output = pd.DataFrame(threeDayPredictOutput)
-    output.to_csv('ans.csv', index=False)
-
-modell()
+threeDayPredict = model.predict(predictX)
+threeDayPredict.shape
+threeDayPredictOutput = scalerYPredict.inverse_transform(threeDayPredict)
+print(threeDayPredictOutput)
+output = pd.DataFrame(threeDayPredictOutput)
+output.to_csv('ans.csv', index=False)
--- test.py
+++ test.py
@@ -2,14 +2,32 @@ import date
 import viz
 import pandas 
 import csv
-def datee():
-    date
-    viz
-    df1 = pandas.read_csv('all.csv').replace("V","30")
-    df2 = pandas.read_csv('viz.csv')
+import model
+
+date
+viz
+df1 = pandas.read_csv('all.csv').replace("V","30")
+df2 = pandas.read_csv('viz.csv')
+res = pandas.concat([df1, df2],axis=1)
+res.to_csv('testdata.csv',index = False)
+
+model
+
+data=[]
+with open('ans.csv', newline='',encoding='utf-8') as csvfile:
+
+     # 讀取 CSV 檔案內容
+    rows = csv.reader(csvfile)
+    for row in rows:
+      
+        data.append(row[0])
+
+ans=data[1]
+print(ans)
 
 
-    res = pandas.concat([df1, df2],axis=1)
 
-    res.to_csv('testdata.csv',index = False)
+
+
+
 
